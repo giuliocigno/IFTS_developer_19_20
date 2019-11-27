dd3c51b414f94d377d1a33c9c826cc3209ad79b9
giuliocigno <57756052+giuliocigno@users.noreply.github.com>
2019-11-27 17:48:11 +0100

    Create 20191127.Colleoni.01.Main.Giulio.java

---
+++ 20191127.Colleoni.01.Main.Giulio.java
@@ -0,0 +1,48 @@
+// sample code, not tested...
+
+public class Programma {
+
+  public static void main(){
+  
+    List<String> strumenti = new ArrayList<>( Arrays.asList("cello","guitar","violin","double bass"));
+    
+    for( int i=0; i < strumenti.lenght; i++ ){
+    
+      List<char> wordnew = new ArrayList<>();  
+      
+      for( int i=0; i < element.lenght; i++ ){
+      
+          current = element.charAt(i); 
+      
+          if( ! isVowel( current) ){
+            
+              wordnew.add(current); 
+          }
+      }
+      
+      //removes element at position i 
+      strumenti.remove(i);
+      //adds element at position i
+      strumenti.add( i, wordnew.toString());  
+    }
+    
+    for( String element : strumenti ){
+      
+      System.out.println(element);  
+    }
+  }
+}
+
+public static boolean isVowel( char c ){
+
+   String vocali = new String("aeiou");
+   
+   if( vocali.indexOf(c) == -1 ){
+      
+      return false; 
+   
+   }else{
+   
+      return true;
+   }
+}
