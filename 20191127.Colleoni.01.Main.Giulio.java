231f6d1f1f5ac7d6c150eaed214c622894da743b
giuliocigno <57756052+giuliocigno@users.noreply.github.com>
2019-11-28 10:06:36 +0100

    Update 20191127.Colleoni.01.Main.Giulio.java

--- 20191127.Colleoni.01.Main.Giulio.java
+++ 20191127.Colleoni.01.Main.Giulio.java
@@ -2,49 +2,50 @@
 
 public class Programma {
 
-  public static void main(){
-  
-    List<String> strumenti = new ArrayList<>( Arrays.asList("cello","guitar","violin","double bass"));
-    
-    for( int i=0; i < strumenti.size(); i++ ){
-    
-      element = strumenti.get(i); 
-      
-      List<char> wordnew = new ArrayList<>();  
-      
-      for( int i=0; i < element.lenght; i++ ){
-      
-          current = element.charAt(i); 
-      
-          if( ! isVowel( current) ){
-            
-              wordnew.add(current); 
-          }
-      }
-      
-      //removes element at position i 
-      strumenti.remove(i);
-      //adds element at position i
-      strumenti.add( i, wordnew.toString());  
-    }
-    
-    for( String element : strumenti ){
-      
-      System.out.println(element);  
-    }
-  }
-}
+  public static void main(String[] args){
+		  
+	    List<String> strumenti = new ArrayList<>( Arrays.asList("cello","guitar","violin","double bass"));
+	    
+	    for( int i=0; i < strumenti.size(); i++ ){
+	    
+	      String element = strumenti.get(i); 
+	      
+	      List<Character> wordnew = new ArrayList<>();  
+	      
+	      for( int j=0; i < element.length(); j++ ){
+	      
+	          Character current = element.charAt(j); 
+	      
+	          if( ! isVowel( current) ){
+	            
+	              wordnew.add(current); 
+	          }
+	      }
+	      
+	      //removes element at position i 
+	      strumenti.remove(i);
+	      //adds element at position i
+	      strumenti.add( i, wordnew.toString());  
+	    }
+	    
+	    for( String element : strumenti ){
+	      
+	      System.out.println(element);  
+	    }
+	  }
+	
 
-public static boolean isVowel( char c ){
+	public static boolean isVowel( Character c ){
 
-   String vocali = new String("aeiou");
-   
-   if( vocali.indexOf(c) == -1 ){
-      
-      return false; 
-   
-   }else{
-   
-      return true;
-   }
+	   String vocali = new String("aeiou");
+	   
+	   if( vocali.indexOf(c) == -1 ){
+	      
+	      return false; 
+	   
+	   }else{
+	   
+	      return true;
+	   }
+	}
 }
