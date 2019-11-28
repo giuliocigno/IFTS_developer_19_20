85865e81b43cf7c9755dc04cefa647be56ae2850
giuliocigno <57756052+giuliocigno@users.noreply.github.com>
2019-11-28 15:31:29 +0100

    Create 20191128.Colleoni.00.CheckOddEven.Giulio.java

---
+++ 20191128.Colleoni.00.CheckOddEven.Giulio.java
@@ -0,0 +1,22 @@
+//sample code, not tested.
+
+public class CheckOddEven {
+
+  public static void main( String[] args) {
+  
+    int number = 49; 
+    
+    System.out.println("The number is " + number);
+    
+    if( number % 2 == 1 ){
+      
+      System.out.println("Odd Number");
+    
+    }else{
+    
+      System.out.println("Even Number");    
+    }
+    
+    System.out.println("BYE!");
+  }
+}
