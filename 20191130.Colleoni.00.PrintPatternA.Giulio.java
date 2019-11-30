e41e24e9156514a87500de5d012e0e0660c8771a
giuliocigno <57756052+giuliocigno@users.noreply.github.com>
2019-11-30 15:24:43 +0100

    Create 20191130.Colleoni.00.PrintPatternA.Giulio.java

---
+++ 20191130.Colleoni.00.PrintPatternA.Giulio.java
@@ -0,0 +1,21 @@
+//tested!!!
+
+//Stampa il pattern A) dell'esercizio PrintPattern nella sezione "Nested loops" del file Esercizi_00.pdf
+
+public class Programma {
+
+    public static void main(String[] args) {
+
+        int lato = 8;
+
+        for( int i=0; i < lato; i++){
+
+            for( int j=0; j < i+1; j++){
+
+                System.out.print("#");
+            }
+
+            System.out.println(); 
+        }
+    }
+}
